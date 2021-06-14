dcc53ae0f97c849048d99d3b1f9e366250215a31
jayChung0302 <whtnek@gmail.com>
2021-06-14 18:21:50 +0900

    Change theconf to hydra in main.py, train.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import argparse
 import logging
 import time
 from torchvision.transforms.transforms import RandomResizedCrop, RandomRotation, Resize
-from tqdm import tqdm
 from datetime import datetime
 
 import numpy as np
@@ -18,81 +17,58 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-from theconf import ConfigArgumentParser
-from theconf import Config as C
+import hydra
+from omegaconf import DictConfig, OmegaConf
 from utils import accuracy, save_checkpoint, create_exp_dir
 from RandAugment import RandAugment
 from model.pyramidnet import PyramidNet
 from train import trainer
 
-parser = ConfigArgumentParser(conflict_handler='resolve')
-parser.add_argument('--data_dir', type=str, help='Dataset directory', default='/dataset')
-parser.add_argument('--exp_name', type=str, default='dryrun', help='denote experiment name')
-parser.add_argument('--checkpoint_path', type=str, default='checkpoint', help='checkpoint path')
-parser.add_argument('--save_path', type=str, default='exp', help='checkpoint path')
-parser.add_argument('--num_epoch', type=int, default=200, help='number of total epochs to run')
-parser.add_argument('--batch_size', type=int, default=128, help='The size of batch')
-parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=1e-3, help='weight decay')
-parser.add_argument('--num_classes', type=int, default=100, help='number of classes')
-parser.add_argument('--cuda', type=int, default=1)
-parser.add_argument('--use_amp', action='store_true', help='using FP16')
-parser.add_argument('--use_lars', action='store_true', help='using layer-wise adaptive rate scaling')
-
-parser.add_argument('--log_cycle', type=float, default=0.1, help='tensorboard logging frequency')
-# continue training
-parser.add_argument('--is_continue', action='store_true', help='continue training')
-parser.add_argument('--load_path', type=str, default=None, help='path for loading checkpoint')
-
 #TODO:
-# Continuing
+# Continuing - get model with config
 # TinyTL
 # progressive learning
 # regularization turn off
 
-args = parser.parse_args()
-if args.use_amp:
-    from apex import amp
-if args.use_lars:
-    from torchlars import LARS
+log = logging.getLogger(__name__)
 
-args.exp_path = os.path.join(args.save_path, args.exp_name)
-create_exp_dir(args.exp_path)
-
-log_format = '%(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_format)
-fh = logging.FileHandler(os.path.join(args.exp_path, 'log.txt'))
-fh.setFormatter(logging.Formatter(log_format))
-logging.getLogger().addHandler(fh)
-
-def main():
-    logging.info(f'Experiment date: {datetime.today().strftime("%Y-%m-%d-%H-%M")}')
-    logging.info(vars(args))
-    writer = SummaryWriter(f'logs/{args.exp_name}')
-    if args.cuda:
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg:DictConfig) -> None:
+    exp_path = os.path.join(cfg.exp.save_path, cfg.exp.exp_name)
+    create_exp_dir(exp_path)
+    if cfg.exp.use_amp:
+        from apex import amp
+    if cfg.exp.use_lars:
+        from torchlars import LARS
+    if cfg.exp.use_cuda:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         pin_memory = True
     else:
         device = torch.device("cpu")
         pin_memory = False
 
-    head = nn.Linear(in_features=1696, out_features=args.num_classes)
+    log.info(OmegaConf.to_yaml(cfg))
+    log.info(f'Experiment date: {datetime.today().strftime("%Y-%m-%d-%H-%M")}')
+    
+    writer = SummaryWriter(f'logs/{cfg.exp.exp_name}')
+    #TODO: get model
+    head = nn.Linear(in_features=1696, out_features=cfg.dataset.num_classes)
     net = PyramidNet(dataset='imagenet', depth=101, alpha=360, num_classes=1000)
     wts = torch.load('./model/pyramidnet101_360.pth')
     net.load_state_dict(wts)
     net.fc = head
-    logging.info(f'network: {net}')
+    log.info(f'network: {net}')
     net = net.to(device)
-
-    criterion = nn.CrossEntropyLoss().to(device)
-
-    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, \
-        weight_decay=args.weight_decay)
-    if args.use_lars:
+    #TODO: get loss_fn
+    loss_fn = nn.CrossEntropyLoss().to(device)
+    #TODO: get optimizer
+    optimizer = torch.optim.SGD(net.parameters(), lr=cfg.params.lr, momentum=cfg.optmizer.momentum, \
+        weight_decay=cfg.optimizer.weight_decay)
+    if cfg.exp.use_lars:
         optimizer = LARS(optimizer)
+    #TODO: get scheduler
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
-    
+    #TODO: get dataset
     # mean = (0.4914, 0.4822, 0.4465)
     # std = (0.2470, 0.2435, 0.2616)
     mean = (0.5071, 0.4865, 0.4409)
@@ -111,13 +87,14 @@ def main():
 			transforms.ToTensor(),
 			transforms.Normalize(mean=mean,std=std)
 		])
+    #TODO: RandAugment config
     # N, M=3, 13
     # train_transform.transforms.insert(0, RandAugment(N, M))
     data_transforms = {'train': train_transform, 'val': val_transform}
-    logging.info(f'{net.__class__.__name__}')
-    logging.info(f'{data_transforms}')
-    logging.info(f'{optimizer}')
-    logging.info(f'{scheduler.__class__.__name__}: {scheduler.state_dict()}')
+    log.info(f'{net.__class__.__name__}')
+    log.info(f'{data_transforms}')
+    log.info(f'{optimizer}')
+    log.info(f'{scheduler.__class__.__name__}: {scheduler.state_dict()}')
 
     # image_datasets = {x: datasets.ImageFolder(os.path.join(args.data_dir, x), data_transforms[x]) \
     #     for x in ['train', 'val']}
@@ -125,12 +102,12 @@ def main():
     image_datasets['train'] = datasets.CIFAR100(root='./dataset', train=True, download=True, transform=data_transforms['train'])
     image_datasets['val'] = datasets.CIFAR100(root='./dataset', train=False, download=True, transform=data_transforms['val'])
 
-    if args.use_amp:
+    if cfg.exp.use_amp:
         # amp initialization
         net, optimizer = amp.initialize(net, optimizer, opt_level="O1")
     
-    net, optimizer, scheduler = trainer(net, image_datasets, args.num_epoch, criterion, optimizer, scheduler, \
-        device, pin_memory, args.exp_path, writer)
+    net, optimizer, scheduler = trainer(net, image_datasets, cfg, loss_fn, optimizer, scheduler, \
+        device, pin_memory, exp_path, writer)
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -6,7 +6,6 @@ import argparse
 import logging
 import time
 from torchvision.transforms.transforms import RandomResizedCrop, RandomRotation, Resize
-from tqdm import tqdm
 from datetime import datetime
 
 import numpy as np
@@ -18,25 +17,25 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-from apex import amp
-from theconf import Config as C
 from utils import save_checkpoint
 from RandAugment import RandAugment
 
-def trainer(net, image_datasets, num_epoch, criterion, optimizer, scheduler, device, pin_memory, exp_path, writer):
+def trainer(net, image_datasets, cfg, loss_fn, optimizer, scheduler, device, pin_memory, exp_path, writer):
+    if cfg.exp.use_amp:
+        from apex import amp
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
-    num_iter = {x: dataset_sizes[x]//C.get()['batch_size'] for x in ['train', 'val']}
-    num_cycle = {x: int(num_iter[x] * C.get()['log_cycle']) for x in ['train', 'val']}
+    num_iter = {x: dataset_sizes[x] // cfg.params.batch_size for x in ['train', 'val']}
+    num_cycle = {x: int(num_iter[x] * cfg.exp.log_cycle) for x in ['train', 'val']}
     logging.info(f'number of iter: {num_iter}')
     logging.info(f'iter cycle for tensorboard: {num_cycle}')
     logging.info('training start ......!')
     training_start_time = time.time()
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=C.get()['batch_size'], \
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=cfg.params.batch_size, \
         shuffle=True, num_workers=4, pin_memory=pin_memory) \
         for x in ['train', 'val']}
     best_acc = 0
-    for epoch in range(1, num_epoch+1):
+    for epoch in range(1, cfg.params.num_epoch+1):
         epoch_start_time = time.time()
         for phase in ['train', 'val']:
             running_loss = 0.0
@@ -55,10 +54,10 @@ def trainer(net, image_datasets, num_epoch, criterion, optimizer, scheduler, dev
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = net(inputs)
                     _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+                    loss = loss_fn(outputs, labels)
 
                     if phase == 'train':
-                        if C.get()['use_amp']:
+                        if cfg.exp.use_amp:
                             with amp.scale_loss(loss, optimizer) as scaled_loss:
                                 scaled_loss.backward()
                         else: loss.backward()
@@ -70,8 +69,8 @@ def trainer(net, image_datasets, num_epoch, criterion, optimizer, scheduler, dev
                 running_corrects += torch.sum(preds == labels.data)
                 if i % num_cycle[phase] == num_cycle[phase]-1:
                     idx = (epoch-1) * num_iter[phase] + i
-                    loss_iters = running_loss/((i+1)*C.get()['batch_size'])
-                    acc_iters = running_corrects/((i+1)*C.get()['batch_size'])
+                    loss_iters = running_loss/((i+1)*cfg.params.batch_size)
+                    acc_iters = running_corrects/((i+1)*cfg.params.batch_size)
                     writer.add_scalar(f'{phase}/loss', loss_iters, idx)
                     writer.add_scalar(f'{phase}/accuracy', acc_iters, idx)
                     for name, weight in net.named_parameters():
@@ -82,7 +81,7 @@ def trainer(net, image_datasets, num_epoch, criterion, optimizer, scheduler, dev
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            logging.info(f'EPOCH:({epoch}/{num_epoch}) {phase} mode || Acc: {epoch_acc:.4f}, Loss: {epoch_loss:.4f} || current best Acc: {best_acc:.4f}')
+            logging.info(f'EPOCH:({epoch}/{cfg.params.num_epochnum_epoch}) {phase} mode || Acc: {epoch_acc:.4f}, Loss: {epoch_loss:.4f} || current best Acc: {best_acc:.4f}')
 
             if phase == 'val':
                 is_best = False
@@ -97,7 +96,7 @@ def trainer(net, image_datasets, num_epoch, criterion, optimizer, scheduler, dev
                         'optimizer_state_dict': optimizer.state_dict(),
                         'scheduler_state_dict': scheduler.state_dict(),}
                 
-                if C.get()['use_amp']:
+                if cfg.exp.use_amp:
                     stats['amp_state_dict'] = amp.state_dict()
 
                 save_checkpoint(stats, is_best, exp_path)
