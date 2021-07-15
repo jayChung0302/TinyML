03085d765dfa737855a4ecb04d9072e0be540ad2
jayChung0302 <whtnek@gmail.com>
2021-07-15 22:32:17 +0900

    wip

---
+++ eval.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+import os, sys
+import logging
+from datetime import datetime
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from utils import accuracy, save_checkpoint, create_exp_dir, load_checkpoint
+from model.pyramidnet import PyramidNet
+from train import trainer
+from model_factory import get_model
+from custom_dataset import get_dataset
+from custom_optimizer import get_optimizer, get_scheduler
+from tinytl import *
+
+
+log = logging.getLogger(__name__)
+
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg:DictConfig) -> None:
+    exp_path = os.path.join(cfg.exp.save_path, cfg.exp.exp_name)
+    create_exp_dir(exp_path)
+    if cfg.exp.use_amp:
+        from apex import amp
+    if cfg.exp.use_cuda:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        pin_memory = True
+    else:
+        device = torch.device("cpu")
+        pin_memory = False
+    log.info(OmegaConf.to_yaml(cfg))
+    writer = SummaryWriter(f'logs/{cfg.exp.exp_name}')
+
+    net = get_model(cfg)
+
+    log.info(f'network: {net}')
+    if cfg.exp.tiny_tl:
+        tinytlb(net)
+    net = net.to(device)
+    loss_fn = nn.CrossEntropyLoss().to(device)
+    optimizer = get_optimizer(net, cfg.optimizer, cfg.params.lr, cfg.exp.use_lars)
+    scheduler = get_scheduler(cfg.scheduler, optimizer)
+    
+    log.info(f'{net.__class__.__name__}')
+    log.info(f'train transform: {cfg.dataset.train_transform}')
+    log.info(f'val transform: {cfg.dataset.val_transform}')
+    log.info(f'{optimizer}')
+    log.info(f'{scheduler.__class__.__name__}: {scheduler.state_dict()}')
+    
+    image_datasets = get_dataset(datacfg=cfg.dataset)
+    
+    if cfg.exp.use_amp:
+        # amp initialization
+        net, optimizer = amp.initialize(net, optimizer, opt_level="O1")
+    
+    net, optimizer, scheduler = trainer(net, image_datasets, cfg, loss_fn, optimizer, scheduler, \
+        device, pin_memory, exp_path, writer)
+
+if __name__ == '__main__':
+    main()
---
+++ kd.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+import os, sys
+import logging
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+from utils import save_checkpoint
+
+def trainer(net, image_datasets, cfg, loss_fn, optimizer, scheduler, device, pin_memory, exp_path, writer, extra=False):
+    if cfg.exp.use_amp:
+        from apex import amp
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+    num_iter = {x: dataset_sizes[x] // cfg.params.batch_size for x in ['train', 'val']}
+    num_cycle = {x: int(num_iter[x] * cfg.exp.log_cycle) for x in ['train', 'val']}
+    logging.info(f'number of iter: {num_iter}')
+    logging.info(f'iter cycle for tensorboard: {num_cycle}')
+    logging.info('training start ......!')
+    training_start_time = time.time()
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=cfg.params.batch_size, \
+        shuffle=True, num_workers=4, pin_memory=pin_memory) \
+        for x in ['train', 'val']}
+    best_acc = 0
+    num_epoch = cfg.params.num_epoch if not extra else cfg.exp.extra_epoch
+    for epoch in range(1, num_epoch+1):
+        epoch_start_time = time.time()
+        for phase in ['train', 'val']:
+            running_loss = 0.0
+            running_corrects = 0
+
+            if phase == 'train':
+                net.train()
+            else:
+                net.eval()
+
+            for i, (inputs, labels) in enumerate(dataloaders[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = net(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = loss_fn(outputs, labels)
+
+                    if phase == 'train':
+                        if cfg.exp.use_amp:
+                            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                                scaled_loss.backward()
+                        else: loss.backward()
+                        optimizer.step()
+                        scheduler.step()
+
+                # loss is already divided by the batch size
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+                if i % num_cycle[phase] == num_cycle[phase]-1:
+                    idx = (epoch-1) * num_iter[phase] + i
+                    loss_iters = running_loss/((i+1)*cfg.params.batch_size)
+                    acc_iters = running_corrects/((i+1)*cfg.params.batch_size)
+                    writer.add_scalar(f'{phase}/loss', loss_iters, idx)
+                    writer.add_scalar(f'{phase}/accuracy', acc_iters, idx)
+                    for name, weight in net.named_parameters():
+                        writer.add_histogram(name, weight, epoch)
+                        writer.add_histogram(f'{name}.grad', weight.grad, epoch)
+                    logging.info(f'Acc: {acc_iters:.4f}, Loss: {loss_iters:.4f}, ({i}/{idx})')
+
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            logging.info(f'EPOCH:({epoch}/{num_epoch}) {phase} mode || Acc: {epoch_acc:.4f}, Loss: {epoch_loss:.4f} || current best Acc: {best_acc:.4f}')
+
+            if phase == 'val':
+                is_best = False
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    is_best = True
+                logging.info('Saving models......')
+                stats = {'last_epoch': epoch,
+                        'acc': epoch_acc,
+                        'loss': epoch_loss,
+                        'net_state_dict': net.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'scheduler_state_dict': scheduler.state_dict(),}
+                
+                if cfg.exp.use_amp:
+                    stats['amp_state_dict'] = amp.state_dict()
+
+                save_checkpoint(stats, is_best, exp_path)
+
+        epoch_duration = time.time() - epoch_start_time
+        logging.info(f'epoch duration: {int(epoch_duration//60)}m {int(epoch_duration)%60:.2f}s')
+    logging.info(f'End of training, whole session took {int((time.time() - training_start_time)//3600)}h, {int((time.time() - training_start_time)//60)}m')
+    logging.info(f'Best validation accuracy: {best_acc}')
+    
+    return net, optimizer, scheduler
